e46f14948555656c8ce5129f6a593abe6e7931c0
gregormaclaine <duckpower333@gmail.com>
2018-12-04 08:48:04 +0000

    added alan2.json, freddie.json, locky3.json

--- sketch.js
+++ sketch.js
@@ -13,8 +13,8 @@ var mapName = "stepan.json";
 var levels = {
   "First": ["austin.json", "stepan.json", "adam.json"],
   "Gregor": ["gregor.json", "Gregor's Keys.json"],
-  "Locky": ["locky2.json", "locky invisimaze.json"],
-  "Alan": ["alan.json"],
+  "Locky": ["locky2.json", "locky3.json", "locky invisimaze.json"],
+  "Alan": ["alan.json", "alan2.json"],
   "Just for Freddie :)": ["freddie.json"]
 };
 var level_names = Object.keys(levels);
